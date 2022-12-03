6b5e36943ed09f87fc7c7a9419cd58132fe5989f
Usama Ghaffar <uk352351@gmail.com>
2022-12-03 06:40:56 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const body = document.querySelector('body')
+const button = document.querySelector('button')
+
+
+
+const intervalId = setInterval(()=> {
+  const red = Math.floor(Math.random() * 126)
+  const green = Math.floor(Math.random() * 126)
+  const blue = Math.floor(Math.random() * 126)
+  const rgb = `rgb(${red}, ${green}, ${blue})`;
+  body.style.background = rgb;
+}, 1000)
+
+button.addEventListener("click", ()=> {
+  clearInterval(intervalId) ;
+  button.textContent = body.style.background
+})
